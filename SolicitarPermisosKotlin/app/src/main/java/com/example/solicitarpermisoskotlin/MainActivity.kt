e47a6a04d0c49f6f56da1b0aaeb6c8088afa7474
Fatima Cordova <fatima150698@gmail.com>
2021-09-01 15:38:55 -0600

    Solicitando permisos y utilizando retrofit

--- SolicitarPermisosKotlin/app/src/main/java/com/example/solicitarpermisoskotlin/MainActivity.kt
+++ SolicitarPermisosKotlin/app/src/main/java/com/example/solicitarpermisoskotlin/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.solicitarpermisoskotlin
 import android.Manifest
 import android.app.AlertDialog
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.solicitarpermisoskotlin.adapter.AdaptadorRecycler
 import com.example.solicitarpermisoskotlin.retrofit.RetrofitInstance
 import com.example.solicitarpermisoskotlin.retrofit.RetrofitServices
 import com.example.solicitarpermisoskotlin.retrofit.model.MessageResponse
@@ -19,32 +22,44 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnEnviar: Button
-    private val PERMISSION = 100
+    private lateinit var recycler : RecyclerView
     private lateinit var listaMensajes: ArrayList<MessageResponse>
-    private val permissions = arrayOf<String>(
-        Manifest.permission.READ_EXTERNAL_STORAGE,
-        Manifest.permission.WRITE_EXTERNAL_STORAGE
-    )
-
+    private lateinit var adaptadorRecycler: AdaptadorRecycler
+    private val PERMISSION = 100
+    private val permissions = arrayOf<String>(Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnEnviar = findViewById(R.id.btnEnviar)
+        initialize()
         solicitarPermisos()
-        btnEnviar.setOnClickListener {
-        }
         prueba()
+    }
+
+    private fun  initialize() {
+        recycler = findViewById(R.id.recycler)
+        initializeRecyclerView()
+    }
+
+    private fun  initializeRecyclerView() {
+        val manager = LinearLayoutManager(this)
         listaMensajes = ArrayList()
+        adaptadorRecycler = AdaptadorRecycler(listaMensajes)
+        recycler.setHasFixedSize(true)
+        recycler.layoutManager = manager
+        recycler.adapter = adaptadorRecycler
+
+
+        val dividerItemDecoration = DividerItemDecoration(recycler.context, manager.orientation)
+        recycler.addItemDecoration(dividerItemDecoration)
     }
 
     private fun solicitarPermisos() {
         // if(ActivityCompat.checkSelfPermission())
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            == PackageManager.PERMISSION_DENIED
-        ) {
+            == PackageManager.PERMISSION_DENIED) {
             alertDialog()
         }
     }
@@ -64,15 +79,13 @@ class MainActivity : AppCompatActivity() {
         ActivityCompat.requestPermissions(this, permissions, PERMISSION);
     }
 
-
     private fun showMessage(message: String = "") {
         val message = if (message.isEmpty()) "No se muestra el msj" else message
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
     override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String?>, grantResults: IntArray
-    ) {
+        requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == PERMISSION) {
             showMessage("Permisos aceptados")
@@ -86,103 +99,23 @@ class MainActivity : AppCompatActivity() {
         responseCall?.enqueue(object: Callback<List<MessageResponse>>{
             override fun onResponse(call: Call<List<MessageResponse>>, response: Response<List<MessageResponse>>) {
                 if(response.body() != null) {
-                    listaMensajes = response.body() as ArrayList<MessageResponse>
+                    val messages = response.body() as ArrayList<MessageResponse>
+                    updateRecycler(messages)
                 }
             }
 
             override fun onFailure(call: Call<List<MessageResponse>>, t: Throwable) {
                 println(t)
             }
-
         })
+    }
 
+    private fun updateRecycler(updateMsjs: ArrayList<MessageResponse>) {
+        if(updateMsjs.size > 0) {
+            listaMensajes.clear()
+            listaMensajes.addAll(updateMsjs)
+            adaptadorRecycler.notifyDataSetChanged()
+        }
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-   if (ActivityCompat.shouldShowRequestPermissionRationale
-                (this, Manifest.permission.WRITE_EXTERNAL_STORAGE) ||
-            (ActivityCompat.shouldShowRequestPermissionRationale
-                (this, Manifest.permission.READ_EXTERNAL_STORAGE))) {
-
-        }
-
-
-
-         private fun solicitarPermisos() {
-        // if(ActivityCompat.checkSelfPermission())
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            == PackageManager.PERMISSION_GRANTED) {
-        }
-        if (ActivityCompat.shouldShowRequestPermissionRationale
-                (this, Manifest.permission.WRITE_EXTERNAL_STORAGE) ||
-            (ActivityCompat.shouldShowRequestPermissionRationale
-                (this, Manifest.permission.READ_EXTERNAL_STORAGE))) {
-            val builder = AlertDialog.Builder(this)
-            builder.setMessage("Se necesitan permisos para continuar...")
-            builder.setPositiveButton(android.R.string.ok) {
-                    dialog, which -> Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
-                dialog.dismiss()
-                ActivityCompat.requestPermissions(this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE);
-            }
-            builder.setNegativeButton("Cancelar", null)
-            builder.show()
-        }
-    }
-
-
-        } else {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
-        }*/
-
-
-/*     if (escribir == PackageManager.PERMISSION_GRANTED && lectura == PackageManager.PERMISSION_GRANTED) {
-    }
-        if (ActivityCompat.shouldShowRequestPermissionRationale
-                (this, Manifest.permission.WRITE_EXTERNAL_STORAGE) ||
-            (ActivityCompat.shouldShowRequestPermissionRationale
-            (this, Manifest.permission.READ_EXTERNAL_STORAGE))) {
-        {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setMessage("Se necesitan permisos de lectura y escritura para continuar...");
-            builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                    finish();
-                }
-            });
-
-            builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                    ActivityCompat.requestPermissions(InicioActivity.this,
-                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, codigo);
-                    ActivityCompat.requestPermissions(InicioActivity.this,
-                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, codigo);
-                }
-            });
-            builder.show();
-        }
-    }
-
-        else{
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, codigo);
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, codigo);
-    }*/
---
+++ SolicitarPermisosKotlin/app/src/main/java/com/example/solicitarpermisoskotlin/adapter/AdaptadorRecycler.kt
@@ -0,0 +1,36 @@
+package com.example.solicitarpermisoskotlin.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.solicitarpermisoskotlin.R
+import com.example.solicitarpermisoskotlin.retrofit.model.MessageResponse
+
+class AdaptadorRecycler (private val newsList: ArrayList<MessageResponse>) :
+    RecyclerView.Adapter<AdaptadorRecycler.MyViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.lista_items, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    inner class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val txtDatosRecycler: TextView = itemView.findViewById(R.id.txtMensajesRecycler)
+
+        fun bind(messageResponse: MessageResponse) {
+            txtDatosRecycler.text = messageResponse.title
+        }
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(newsList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return newsList.size
+    }
+
+}
